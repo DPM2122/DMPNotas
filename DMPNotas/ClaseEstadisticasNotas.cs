2b074d42b5fcb3842f40c08f496c8a25429d51da
Desi MP <desmp7bis@gmail.com>
2022-06-23 18:19:56 +0200

    Proyecto documentado

--- DMPNotas/ClaseEstadisticasNotas.cs
+++ DMPNotas/ClaseEstadisticasNotas.cs
@@ -6,20 +6,30 @@ using System.Threading.Tasks;
 
 namespace DMPNotas
 {
-   
+    /// <summary>
+    ///<para>Clase que calcula las estadíaticas de un conjunto de notas</para> 
+    ///<para>Contabiliza el número de suspensos, aprobados, notables y sobresalientes</para>
+    /// </summary>
     public class ClaseEstadisticasNotas //esta clase nos calcula las estadisticas de un conjunto de notas
     {
-        public const int kSuspensos = 5;
-        public const int kAprobados = 7;
-        public const int kNotables = 9;
-
+        /// <summary>
+        /// Variables constantes para determinar el tope de notas para las calificaciones
+        /// </summary>
+        public const int TopeSuspensos = 5;
+        public const int TopeAprobados = 7;
+        public const int TopeNotables = 9;
+        /// <summary>
+        /// Declaración de variables
+        /// </summary>
         private int suspensos;
         private int aprobados;
         private int notables;
         private int sobresalientes;
         private double media;
        
-
+        /// <summary>
+        /// Se inician los getters y los setters
+        /// </summary>
         public int Suspensos
         {
             get { return suspensos; }
@@ -47,6 +57,9 @@ namespace DMPNotas
             set { media = value; }
         }
 
+        /// <summary>
+        /// Se inician las variables a 0
+        /// </summary>
         public ClaseEstadisticasNotas ()
         {
             Suspensos = 0;
@@ -56,10 +69,23 @@ namespace DMPNotas
             Media = 0;
         }
 
+        /// <summary>
+        /// Se inicia la Lista donde se van a almacenar las notas que se introduzcan
+        /// </summary>
+        /// <param name="ListaNotas">La notas de los alumnos que se irán introduciendo</param>
+        /// <remarks>Se  hace la llamada al método CalcularEstadísticas</remarks>
         public ClaseEstadisticasNotas(List<int> ListaNotas)
         {
             CalcularEstadisticas(ListaNotas);
         }
+
+        /// <summary>
+        /// Método calcula la estadistica de un conjunto de notas 
+        /// </summary>
+        /// <param name="ListaNotas">Notas que se van introduciendo</param>
+        /// <remarks>Y contabiliza el número de suspensos, aprobados, notables y sobresalientes de la lista de notas y la media de dicha lista de notas</remarks>
+        /// <returns>Se obtiene el número de suspensos<see cref=">Suspensos"/>, aprobados<see cref=">Aprobados"/>, notables<see cref=">Notables"/>
+        /// y sobresalientes<see cref=">Sobresalientes"/> de la lista de notas y la media<see cref=">Media"/> de dicha lista de notas</returns>
 
         private void CalcularEstadisticas(List<int> ListaNotas)
         {
@@ -67,19 +93,25 @@ namespace DMPNotas
 
             for (int i = 0; i < ListaNotas.Count; i++)
             {
-                if (ListaNotas[i] < kSuspensos) Suspensos++;
-                else if (ListaNotas[i] >= kSuspensos && ListaNotas[i] < kAprobados) Aprobados++;
-                else if (ListaNotas[i] >= kAprobados && ListaNotas[i] < kNotables) Notables++;
-                else if (ListaNotas[i] >= kNotables) Sobresalientes++;
+                if (ListaNotas[i] < TopeSuspensos) Suspensos++;
+                else if (ListaNotas[i] >= TopeSuspensos && ListaNotas[i] < TopeAprobados) Aprobados++;
+                else if (ListaNotas[i] >= TopeAprobados && ListaNotas[i] < TopeNotables) Notables++;
+                else if (ListaNotas[i] >= TopeNotables) Sobresalientes++;
                 Media = Media + ListaNotas[i];
             }
             Media = Media / ListaNotas.Count;
         }
 
+        /// <summary>
+        /// Método que calcula media de una lista de notas
+        /// </summary>
+        /// <param name="ListaNotas">Lista donde se almacenan las notas introducidas</param>
+        /// <returns>Nos devuelve la media de todas las notas que se introducen en la lista<see cref=">ListaNotas"/></returns>
+        /// <exception cref="Exception">Salta una excepción si la lista está vacía</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Salta la excepción si los valores introducidos son menor de 0 o mayor de 10</exception>
         public double CalcularMedia (List<int> ListaNotas)
         {
            
-
             if (ListaNotas.Count <= 0)
             {
                 throw new Exception("La lista no puede estar vacía");
@@ -97,7 +129,5 @@ namespace DMPNotas
 
             return Media;
         }
-
-
     }
 }
